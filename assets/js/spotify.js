8b190138701b8ca3f257ca71f371b2cc753f33fe
rst_979 <richard.taing@gmail.com>
2019-08-03 22:34:13 +1000

    Added top tracks to list

--- assets/js/spotify.js
+++ assets/js/spotify.js
@@ -18,7 +18,7 @@ const authEndpoint = "https://accounts.spotify.com/authorize";
 // Replace with your app's client ID, redirect URI and desired scopes
 const clientId = "8eb9c5f7f3b048bc9184193d33afdc96";
 const redirectUri = "http://localhost:8080";
-const scopes = ["user-top-read"];
+const scopes = ["user-top-read", "user-read-recently-played"];
 
 // If there is no token, redirect to Spotify authorization
 if (!_token) {
@@ -39,6 +39,36 @@ $.ajax({
     data.items.map(function(artist) {
       let item = $("<li>" + artist.name + "</li>");
       item.appendTo($("#top-artists"));
+    });
+  }
+});
+
+$.ajax({
+  url: "https://api.spotify.com/v1/me/top/tracks",
+  type: "GET",
+  beforeSend: function(xhr) {
+    xhr.setRequestHeader("Authorization", "Bearer " + _token);
+  },
+  success: function(data) {
+    // Do something with the returned data
+    data.items.map(function(artist) {
+      let item = $("<li>" + artist.name + "</li>");
+      item.appendTo($("#top-tracks"));
+    });
+  }
+});
+
+$.ajax({
+  url: "https://api.spotify.com/v1/me/top/currently-playing",
+  type: "GET",
+  beforeSend: function(xhr) {
+    xhr.setRequestHeader("Authorization", "Bearer " + _token);
+  },
+  success: function(data) {
+    // Do something with the returned data
+    data.items.map(function(artist) {
+      let item = $("<li>" + artist.name + "</li>");
+      item.appendTo($("#top-plays"));
     });
   }
 });
