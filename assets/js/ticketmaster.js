3bc84148b186dfad56440f9f929668208c37f0f4
unknown <uchpolinet@gmail.com>
2019-08-03 20:06:27 +1000

    created a function to call ticketmaster api

--- assets/js/ticketmaster.js
+++ assets/js/ticketmaster.js
@@ -9,3 +9,21 @@ console.log(params.toString());
 // client_id=&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2F
 
 // next step - anchor token
+
+// function to call and display events from Ticketmaster
+function eventFromTicketMaster() {
+  var eventInfo = $(this).attr("data-event");
+
+  //queryURL is the url we'll use to query the API
+  var queryURL =
+    "https://app.ticketmaster.com/discovery/v2/events.json?apikey=COsXEH07ztMABw0SgNFNxALf8IefVSt3&countryCode=au&keyword=" +
+    eventInfo;
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(URL);
+    console.log(response);
+  });
+}
+eventFromTicketMaster();
